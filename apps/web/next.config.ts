06855b28cb43ac230f32336c71b9fadf3707e625
parthvi67 <parth@eh.com>
2026-05-20 12:59:44 +0000

    feat: initialize API backend and refactor web report and dashboard components

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["ai-cfo.engineershut.com"],
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
   },
